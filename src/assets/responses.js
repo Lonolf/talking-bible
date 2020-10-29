31392dd796122f1f45e61021efe139bfebb47520
Lonolf <gabriele.pini@bytes.black>
2020-10-29 19:53:13 +0100

    cleaning

--- src/assets/responses.js
+++ src/assets/responses.js
@@ -1,6 +1,7 @@
-export default ({ character: { name = '', problem = '', sons = [] } = {} }) => ([
+export default ({ character: { name = '', problem = '', sons = [], domicile } = {} }) => ([
   ['identity', 'ciao', 'Ciao, piacere di conoscerti'],
   ['identity', 'salve', 'Oh oh, amico amico, salve lo dici a tua sorella! 😉'],
+  ['identity', 'dove abiti', domicile == null ? 'Non ho fissa dimora' : `Io abito a ${domicile}`],
   ['includes', 'nome', `Il mio nome è ${name}`],
   ['includes', 'chiami', `Il mio nome è ${name}`],
   ['includes', 'problema', problem !== '' ? `Il mio problema è che ${problem}` : 'Nulla di particolare, grazie'],
