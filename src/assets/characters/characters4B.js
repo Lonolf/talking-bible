05e39fb5198eb46e52b26238e8127f7b7de98279
Lonolf <gabriele.pini@bytes.black>
2020-10-29 20:14:21 +0100

    cleanign

--- src/assets/characters/characters4B.js
+++ src/assets/characters/characters4B.js
@@ -1,33 +1,31 @@
 const Terach = {
-    name: 'Terach',
-    surname: null,
-    sons: ['Abramo',],
-  }
-  
-  const Abramo = {
-    default: true,
-    name: 'Abramo',
-    surname: null,
-  
-    father: 'Terach',
-    sons: ['Ismaele', 'Isacco'],
-    wifes: ['Sarah'],
-  
-    birthday: null,
-    deathDay: null,
-    century: 'XVIII a.C.',
-  
-    motherland: 'Mesopotamia',
-    domicile: null,
-    profession: 'Pastore',
-    status: 'capotribù',
-  
-    problem: 'Nonostante io e mia moglie siamo sposati da molti anni, non abbiamo figli maschi.'
-  }
+  name: 'Terach',
+  surname: null,
+  sons: ['Abramo'],
+}
 
+const Abramo = {
+  default: true,
+  name: 'Abramo',
+  surname: null,
 
-  export default {
-    Terach,
-    Abramo,
-  }
-  
+  father: 'Terach',
+  sons: ['Ismaele', 'Isacco'],
+  wifes: ['Sarah'],
+
+  birthday: null,
+  deathDay: null,
+  century: 'XVIII a.C.',
+
+  motherland: 'Mesopotamia',
+  domicile: null,
+  profession: 'Pastore',
+  status: 'capotribù',
+
+  problem: 'Nonostante io e mia moglie siamo sposati da molti anni, non abbiamo figli maschi.',
+}
+
+export default {
+  Terach,
+  Abramo,
+}
