81c4722cac46b89abf57d75824d03311d2d1ea2f
Lonolf <gabriele.pini@bytes.black>
2020-11-05 19:39:14 +0100

    cleaning

--- src/assets/characters/characters4B.js
+++ src/assets/characters/characters4B.js
@@ -25,7 +25,31 @@ const Abramo = {
   problem: 'Nonostante io e mia moglie siamo sposati da molti anni, non abbiamo figli maschi.',
 }
 
+const Sarah = {
+  name: 'Sarah',
+  husband: 'Abramo',
+  sons: ['Isacco'],
+  problem: 'Ho paura che Ismaele soppianti Isacco',
+}
+
+const Ismaele = {
+  name: 'Ismaele',
+  father: 'Abramo',
+  mother: 'Agar',
+  problem: 'Non sono figlio di Sarah, ma di una schiava',
+}
+
+const Isacco = {
+  name: 'Isacco',
+  father: 'Abramo',
+  mother: 'Sarah',
+  problem: 'Mio padre era pronto a sacrificarmi a Dio',
+}
+
 export default {
   Terach,
   Abramo,
+  Sarah,
+  Ismaele,
+  Isacco,
 }
