c35db8b5371d4fc481c734b349712f34b06667b1
Lonolf <gabriele.pini@bytes.black>
2020-11-05 08:40:36 +0100

    wifes question

--- src/assets/responses.js
+++ src/assets/responses.js
@@ -1,8 +1,18 @@
-export default ({ character: { name = '', problem = '', sons = [], domicile, profession, importance } = {} }) => ([
+export default ({
+  character: {
+    name = '',
+    problem = '',
+    sons = [],
+    wifes = [],
+    domicile,
+    profession,
+    importance
+  } = {} }) => ([
   ['identity', 'ciao', 'Ciao, piacere di conoscerti'],
   ['identity', 'salve', 'Oh oh, amico amico, salve lo dici a tua sorella! 😉'],
   ['includes', 'importante', importance == null ? 'Mi dispiace, devi scoprirlo tu.' : importance],
   ['identity', 'dove abiti', domicile == null ? 'Non ho fissa dimora' : `Io abito a ${domicile}`],
+  ['includes', 'moglie', wifes.length === 0 ? 'Non sono sposato' : wifes.length === 1 ? `Ho una moglie, ${wifes[0]}` : `Ho ${wifes.length} mogli, ${wifes.join(', ')}`],
   ['includes', 'lavoro', profession == null ? 'Sono disoccupato' : `Il mio lavoro è fare il ${profession.toLowerCase()}`],
   ['includes', 'nome', `Il mio nome è ${name}`],
   ['includes', 'chiami', `Il mio nome è ${name}`],
